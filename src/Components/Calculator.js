f63bbbb3918ed9a9ab6b40c6e5e52c54509dc312
mickyngub <57980116+mickyngub@users.noreply.github.com>
2020-07-28 16:08:25 +0700

    sign/num/ac PressHandler

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+
 import "./Calculator.css";
 const Calculator = () => {
+  const [wholeString, setWholeString] = useState("");
+  const [value, setValue] = useState(0);
+
+  const signPressHandler = (sign) => {
+    setValue((current) => (current = sign));
+  };
+  const numberPressHandler = (number) => {
+    if (value === 0) {
+      setValue(number);
+      setWholeString(number);
+    } else {
+      setValue((current) => current + number);
+      setWholeString((current) => current + number);
+    }
+  };
+  const acPressHandler = () => {
+    setValue(0);
+    setWholeString("");
+  };
   return (
     <Container
       maxWidth="sm"
@@ -18,54 +37,65 @@ const Calculator = () => {
     >
       <div className="grid-container">
         <div className="grid-item showNumber">
-          <Typography>number goes here</Typography>
+          <Typography>whole string goes here {wholeString}-----</Typography>
+
+          <Typography>current number goes here {value}</Typography>
         </div>
-        <div className="grid-item AC">
+        <div className="grid-item AC" onClick={acPressHandler}>
           <Typography>AC</Typography>
         </div>
-        <div className="grid-item s-divide">
+        <div
+          className="grid-item s-divide"
+          onClick={() => signPressHandler("/")}
+        >
           <Typography>/</Typography>
         </div>
-        <div className="grid-item s-multiply">
+        <div
+          className="grid-item s-multiply"
+          onClick={() => signPressHandler("X")}
+        >
           <Typography>X</Typography>
         </div>
-        <div className="grid-item no7">
+        <div className="grid-item no7" onClick={() => numberPressHandler("7")}>
           <Typography>7</Typography>
         </div>
-        <div className="grid-item no8">
+        <div className="grid-item no8" onClick={() => numberPressHandler("8")}>
           <Typography>8</Typography>
         </div>
-        <div className="grid-item no9">
+        <div className="grid-item no9" onClick={() => numberPressHandler("9")}>
           <Typography>9</Typography>
         </div>
-        <div className="grid-item s-minus">
+        <div
+          className="grid-item s-minus"
+          onClick={() => signPressHandler("-")}
+        >
           <Typography>-</Typography>
         </div>
-        <div className="grid-item no4">
+        <div className="grid-item no4" onClick={() => numberPressHandler("4")}>
           <Typography>4</Typography>
         </div>
-        <div className="grid-item no5">
+        <div className="grid-item no5" onClick={() => numberPressHandler("5")}>
           <Typography>5</Typography>
         </div>
-        <div className="grid-item no6">
+        <div className="grid-item no6" onClick={() => numberPressHandler("6")}>
           <Typography>6</Typography>
         </div>
-        <div className="grid-item s-plus">
+        <div className="grid-item s-plus" onClick={() => signPressHandler("+")}>
           <Typography>+</Typography>
         </div>
-        <div className="grid-item no1">
+        <div className="grid-item no1" onClick={() => numberPressHandler("1")}>
           <Typography>1</Typography>
         </div>
-        <div className="grid-item no2">
+        <div className="grid-item no2" onClick={() => numberPressHandler("2")}>
           <Typography>2</Typography>
         </div>
-        <div className="grid-item no3">
+        <div className="grid-item no3" onClick={() => numberPressHandler("3")}>
           <Typography>3</Typography>
         </div>
         <div className="grid-item s-equal">
           <Typography>=</Typography>
         </div>
-        <div className="grid-item no0">
+        <div className="grid-item no0" onClick={() => numberPressHandler("0")}>
           <Typography>0</Typography>
         </div>
         <div className="grid-item s-dot">
